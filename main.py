cb8296a9e16c3222b33ccb3df8ebd52d0c6a1032
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2020-11-28 17:47:14 -0800

    Add basic movement

--- main.py
+++ main.py
@@ -1,35 +1,51 @@
 import argparse
 import string
 import mmap
+import os
 
 
+class Foo:
+    def __init__(self, input_file: str):
+        self.input_file = input_file
 
-def main(input_file):
-    with open(input_file, "r+b") as file:
-        mm = mmap.mmap(file.fileno(), 0)
-        y = 0
-        for x in range(1000):
-            print("".join(
-                chr(mm[y+x]) if chr(mm[y+x]) in string.printable
-                else "."
-                for x in range(16)
-            ))
-            y += 16
+    def render(self, start: int = 0, buff: int = 10):
+        self.start = start
+        self.buff = buff
+        self.end = start + buff
 
+        for x in self.get_hex():
+            print(x)
 
-def maino(input_file):
-    with open(input_file, "r+b") as file:
-        mm = mmap.mmap(file.fileno(), 0)
-        y = 0
-        for x in range(1000):
-            layer = ""
-            for x in range(16):
-                if (char := chr(mm[y+x])) in string.printable:
-                    layer += char
-                else:
-                    layer += "."
-            y += 16
-            print(layer)
+    def get_hex(self) -> list:
+        with open(self.input_file, "r+b") as file:
+            mm = mmap.mmap(file.fileno(), 0)
+            pointer = self.start
+
+            for layer_num in range(self.start, self.end):
+                layer = ""
+                for x in range(16):
+                    if (char := chr(mm[pointer+x])) in string.printable:
+                        layer += char
+                    else:
+                        layer += "."
+                pointer += 16
+                yield layer
+
+def main(args):
+    foo = Foo(args.i)
+    foo.render()
+
+    start = 0
+    buff = 10
+    while 1:
+        check = input("> ")
+        os.system("clear")
+        if check == "j":
+            start -= 16
+            foo.render(start, buff)
+        if check == "k":
+            start += 16
+            foo.render(start, buff)
 
 
 def parse_args():
@@ -40,4 +56,4 @@ def parse_args():
 
 if __name__ == "__main__":
     args = parse_args()
-    main(args.i)
+    main(args)
